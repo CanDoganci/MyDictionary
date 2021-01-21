50fe9f3d1d21bdb592abe49325647e73f27125fb
can doğanci <canndoganci@hotmail.com>
2021-01-21 22:38:57 +0300

    MyDictionary

---
+++ MyDictionary.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<T>
+    {
+        T[] tables;
+
+        public MyDictionary()
+        {
+            tables = new T[0];
+        }
+
+        public void Add(T table)
+        {
+            T[] tempTables = tables;
+            tables = new T[tables.Length + 1];
+
+            for (int i = 0; i < tempTables.Length; i++)
+            {
+                tables[i] = tempTables[i];
+            }
+
+            tables[tables.Length - 1] = table;
+        
+      
+        }   
+         public T[] table
+        {
+            get { return tables; }
+        }   
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string> _tables = new MyDictionary<string>();
+            _tables.Add("Customer");
+            _tables.Add("Assets");
+            _tables.Add("Payment");
+            _tables.Add("Relation");
+
+
+            foreach (var item in _tables.table)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
